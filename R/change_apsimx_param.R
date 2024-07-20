8794b0f9fb1412092f1bb882c154d9e6a79b8369
Luana Becker da Luz <luanabeckerdaluz@gmail.com>
2024-07-20 18:30:25 -0300

    :sparkles: Add support to some XYPairs variables

--- R/change_apsimx_param.R
+++ R/change_apsimx_param.R
@@ -48,13 +48,32 @@ change_apsimx_param <- function(exe, file_to_run, param_values) {
 #'
 #' @export
 #'
+#' 
+
+PATH_VEGTHETIMRES_X3 <- ".Simulations.Replacements.Soybean.Phenology.VegetativeThermalTime.Response.X" # 10, 20, X, 40
+PATH_REPTHETIMRES_X3 <- ".Simulations.Replacements.Soybean.Phenology.ReproductiveThermalTime.Response.X"  # 10, 15, X, 40
+PATH_VEGPHOMOD_X1 <- ".Simulations.Replacements.Soybean.Phenology.VegetativePhotoperiodModifier.XYPairs.X" # X, 21.19
+PATH_REPPHOMOD_X1 <- ".Simulations.Replacements.Soybean.Phenology.ReproductivePhotoperiodModifier.XYPairs.X" # X, 21.19
+PATH_EXTCOEFFXY_Y1 <- ".Simulations.Replacements.Soybean.Leaf.ExtinctionCoefficient.XYPairs.Y" # 0.6, 0.4
+
 generate_config_file <- function(param_values) {
   config_file <- tempfile('apsimOnR', fileext = '.conf')
   parameter_names <- names(param_values)
   fileConn <- file(config_file)
   lines <- vector("character", length(param_values))
-  for (i in 1:length(param_values))
-    lines[i] <- paste(parameter_names[i], '=', as.character(param_values[i]))
+  for (i in 1:length(param_values)) {
+    param_name <- parameter_names[i]
+    param_value <- as.character(param_values[i])
+    lines[i] <- switch(
+      param_name,
+      PATH_VEGTHETIMRES_X3 = paste(param_name, "= 10, 20,", param_value, ", 40"),
+      PATH_REPTHETIMRES_X3 = paste(param_name, "= 10, 15,", param_value, ", 40"),
+      PATH_VEGPHOMOD_X1 = paste(param_name, "=", param_value, ", 21.19"),
+      PATH_REPPHOMOD_X1 = paste(param_name, "=", param_value, ", 21.19"),
+      PATH_EXTCOEFFXY_Y1 = paste(param_name, "=", param_value, ", 0.4"),
+      paste(param_name, "=", param_value)
+    )
+  }
   writeLines(lines, fileConn)
   close(fileConn)
   return(config_file)
